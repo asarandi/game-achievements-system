c553557a277bf6d37fce088c1bc0a736c52c2f67
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-08-01 02:49:47 -0700

    server parses request as json

--- listen.go
+++ listen.go
@@ -5,6 +5,7 @@ import (
     "log"
     "fmt"
     "net/http"
+    "encoding/json"
 )
 
 //
@@ -14,11 +15,35 @@ import (
 
 
 
+type CreateAchievementRequest struct {
+    Slug            string  `json:"slug"`
+    Title           string  `json:"title"`
+    Description     string  `json:"description"`
+    Img             string  `json:"img"`
+}
+
+type ResponseError struct {
+    Result  string  `json:"result"`
+    Info    string  `json:"info"`
+}
+
 func createAchievementHandler(w http.ResponseWriter, r *http.Request) {
     body := make([]byte, r.ContentLength)
     n, err := r.Body.Read(body)
     if err == io.EOF && n > 0  {
-        fmt.Println(string(body))
+        w.Header().Set("content-type", "application/json")
+        var req CreateAchievementRequest
+        if err = json.Unmarshal(body, &req); err != nil {
+            js, _ := json.Marshal(ResponseError{"error", err.Error()})
+            w.Write(js)
+            return
+        }
+        if len(req.Slug) < 1 {
+            js, _ := json.Marshal(ResponseError{"error", "slug cannot be empty"})
+            w.Write(js)
+            return
+        }
+        fmt.Println(req)
     }
 }
 
