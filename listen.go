6c0b36082cdf253948c5461cf3d1bb0ce1d3c39b
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-08-01 01:21:13 -0700

    listen and print

--- listen.go
+++ listen.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    import "log"
-    import "fmt"
-    import "net/http"
+    "io"
+    "log"
+    "fmt"
+    "net/http"
 )
 
 //
@@ -13,13 +14,16 @@ import (
 
 
 
+func createAchievementHandler(w http.ResponseWriter, r *http.Request) {
+    body := make([]byte, r.ContentLength)
+    n, err := r.Body.Read(body)
+    if err == io.EOF && n > 0  {
+        fmt.Println(string(body))
+    }
+}
 
 func main() {
-    http.Handle("/createAchievement", createAchievementHandler)
-
-    http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
+    http.HandleFunc("/createAchievement", createAchievementHandler)
 
     log.Fatal(http.ListenAndServe(":4242", nil))
 }
