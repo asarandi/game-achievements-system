87450597aba800579733d6d8bf71cced46b5f278
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-08-02 03:30:05 -0700

    getAllRecords()

--- listen.go
+++ listen.go
@@ -13,22 +13,22 @@ import (
 
 type Achievement struct {
     gorm.Model
-    Slug            string  `gorm:"unique" json:"slug"`
-    Name            string  `gorm:"unique" json:"name"`
-    Desc            string  `json:"desc"`
-    Img             string  `json:"img"`
+    Slug            string      `gorm:"unique" json:"slug"`
+    Name            string      `gorm:"unique" json:"name"`
+    Desc            string      `json:"desc"`
+    Img             string      `json:"img"`
 }
 
 type Member struct {
     gorm.Model
-    Name            string  `gorm:"unique" json:"name"`
-    Img             string  `json:"img"`
+    Name            string      `gorm:"unique" json:"name"`
+    Img             string      `json:"img"`
 }
 
 type Team struct {
     gorm.Model
-    Name            string  `gorm:"unique" json:"name"`
-    Img             string  `json:"img"`
+    Name            string      `gorm:"unique" json:"name"`
+    Img             string      `json:"img"`
 }
 
 type Response struct {
@@ -46,6 +46,14 @@ func jsonResponse(w http.ResponseWriter, r Response) {
     json.NewEncoder(w).Encode(r)
 }
 
+func translateError(code int, msg string) (int, string) {
+    switch {
+    case strings.Contains(msg, "UNIQUE constraint failed"): msg = "record already exists"; code = http.StatusNotAcceptable
+    case msg == "record not found": code = http.StatusNotFound
+    }
+    return code, msg
+}
+
 func createRecord(w http.ResponseWriter, r *http.Request, model interface{}) {
     decoder := json.NewDecoder(r.Body)
     if err := decoder.Decode(model); err != nil {
@@ -60,18 +68,19 @@ func createRecord(w http.ResponseWriter, r *http.Request, model interface{}) {
     jsonResponse(w, Response{true, http.StatusCreated, "ok", nil})
 }
 
-func translateError(code int, msg string) (int, string) {
-    switch {
-    case strings.Contains(msg, "UNIQUE constraint failed"): msg = "record already exists"; code = http.StatusNotAcceptable
-    case msg == "record not found": code = http.StatusNotFound
-    }
-    return code, msg
-}
-
 func getRecord(w http.ResponseWriter, r *http.Request, model interface{}) {
     vars := mux.Vars(r)
     id := vars["id"]
     if err := DB.First(model, id).Error; err != nil {
+        errorCode, errorMessage := translateError(http.StatusInternalServerError, err.Error())
+        jsonResponse(w, Response{false, errorCode, errorMessage, nil})
+        return
+    }
+    jsonResponse(w, Response{true, http.StatusOK, "ok", model})
+}
+
+func getAllRecords(w http.ResponseWriter, r *http.Request, model interface{}) {
+    if err := DB.Find(model).Error; err != nil {
         errorCode, errorMessage := translateError(http.StatusInternalServerError, err.Error())
         jsonResponse(w, Response{false, errorCode, errorMessage, nil})
         return
@@ -103,7 +112,17 @@ func getTeam(w http.ResponseWriter, r *http.Request) {
     getRecord(w, r, &Team{})
 }
 
+func getAllAchievements(w http.ResponseWriter, r *http.Request) {
+    getAllRecords(w, r, &[]Achievement{})
+}
 
+func getAllMembers(w http.ResponseWriter, r *http.Request) {
+    getAllRecords(w, r, &[]Member{})
+}
+
+func getAllTeams(w http.ResponseWriter, r *http.Request) {
+    getAllRecords(w, r, &[]Team{})
+}
 
 func dbInit() *gorm.DB {
     db, err := gorm.Open("sqlite3", "database.sqlite")
@@ -117,12 +136,18 @@ func dbInit() *gorm.DB {
 func main() {
     DB = dbInit()
     r := mux.NewRouter()
-    r.HandleFunc("/achievements", createAchievement).Methods("POST")
-    r.HandleFunc("/achievements/{id:[0-9]+}", getAchievement).Methods("GET")
+
+    r.HandleFunc("/achievements", createAchievement).Methods("POST")                // create new record
+    r.HandleFunc("/achievements/{id:[0-9]+}", getAchievement).Methods("GET")        // get record by id
+    r.HandleFunc("/achievements", getAllAchievements).Methods("GET")                // get all records
+
     r.HandleFunc("/members", createMember).Methods("POST")
     r.HandleFunc("/members/{id:[0-9]+}", getMember).Methods("GET")
+    r.HandleFunc("/members", getAllMembers).Methods("GET")
+
     r.HandleFunc("/teams", createTeam).Methods("POST")
     r.HandleFunc("/teams/{id:[0-9]+}", getTeam).Methods("GET")
+    r.HandleFunc("/teams", getAllTeams).Methods("GET")
 
     fmt.Println("listening on :4242")
     log.Fatal(http.ListenAndServe(":4242", r))
