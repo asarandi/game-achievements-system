57c60c3873d17a891ca88c2b982d36cf518d77ff
Alexandr SARANDI <sarandi.alex@gmail.com>
2019-08-07 00:41:44 -0700

    minify bool logic in achievements

--- achievements.go
+++ achievements.go
@@ -12,40 +12,24 @@ var ASF = []AchievementSlugFunction {
 
 // “sharpshooter award” – a user receives this for landing 75% of their attacks, assuming they have at least attacked once.
 func isSharpshooterAward(stat Stat) bool {
-    if (stat.NumAttacks > 0) {
-        if (stat.NumHits * 100 / stat.NumAttacks) >= 75 {
-            return true
-        }
-    }
-    return false
+    return (stat.NumAttacks > 0) && ((stat.NumHits * 100 / stat.NumAttacks) >= 75)
 }
 
 //“bruiser” award – a user receives this for doing more than 500 points of damage in one game
 func isBruiserAward(stat Stat) bool {
-    if (stat.AmountDamage >= 500) {
-        return true
-    }
-    return false
+    return stat.AmountDamage >= 500
 }
 
 //“veteran” award – a user receives this for playing more than 1000 games in their lifetime.
 func isVeteranAward(stat Stat) bool {
     var count int
-    if  DB.Find(&Stat{}, Stat{MemberID: stat.MemberID}).Count(&count).Error == nil {
-       if count >= 1000 {
-            return true
-       }
-    }
-    return false
+    DB.Find(&Stat{}, Stat{MemberID: stat.MemberID}).Count(&count)
+    return count >= 1000
 }
 
 //“big winner” award – a user receives this for having 200 wins
 func isBigwinnerAward(stat Stat) bool {
     var count int
-    if  DB.Find(&Stat{}, Stat{MemberID: stat.MemberID, IsWinner: true}).Count(&count).Error == nil {
-       if count >= 200 {
-            return true
-       }
-    }
-    return false
+    DB.Find(&Stat{}, Stat{MemberID: stat.MemberID, IsWinner: true}).Count(&count)
+    return count >= 200
 }
